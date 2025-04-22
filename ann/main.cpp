7261a4231a9d53c2eb81bc0a493db771a50e34a9
Анна Архангельская <arhfotina@gmail.com>
2025-04-22 14:06:14 +0300

    ненужная фигня

--- ann/main.cpp
+++ ann/main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include "../../Aspose.Words.Cpp_25.3/Aspose.Words.Cpp/include/Aspose.Words.Cpp/Document.h"
+#include "../../Aspose.Words.Cpp_25.3/Aspose.Words.Cpp/include/Aspose.Words.Cpp/SaveFormat.h"
 
-using namespace std;
 
-int main(){
-    std::cout << "HELLO world!!!";
+using namespace Aspose::Words;
+
+int main() {
+    // Загрузка документа
+    System::SharedPtr<Document> doc = System::MakeObject<Document>(u"example.docx");
+
+    // Сохранение в другом формате
+    doc->Save(u"output.pdf", SaveFormat::Pdf);
+
     return 0;
 }
-
